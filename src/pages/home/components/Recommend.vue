25bd72b931810d5309572442bdebaec7ef3513d4
stu <stu@sina.com>
2019-08-12 12:00:16 +0800

    列表部分

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,91 @@
+<template>
+	<div>
+		<div class="title">热销推荐</div>
+		<ul>
+			<li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+					<img class="item-img" :src="item.imgUrl" />
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+					<button class="item-button">查看详情</button>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name:'HomeRecommend',
+	data () {
+		return {
+			recommendList: [{
+				id: '0001',
+				imgUrl: 'https://imgs.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_250x250_6ec2f798.jpg',
+				title: '北京野生动物园',
+				desc: '敢与森林之王近距离接触吗？'
+			},
+			{
+				id: '0002',
+				imgUrl: 'https://imgs.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_250x250_6ec2f798.jpg',
+				title: '北京野生动物园',
+				desc: '敢与森林之王近距离接触吗？'
+			},
+			{
+				id: '0003',
+				imgUrl: 'https://imgs.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_250x250_6ec2f798.jpg',
+				title: '北京野生动物园',
+				desc: '敢与森林之王近距离接触吗？'
+			},
+			{
+				id: '0004',
+				imgUrl: 'https://imgs.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_250x250_6ec2f798.jpg',
+				title: '北京野生动物园',
+				desc: '敢与森林之王近距离接触吗？'
+			},
+			{
+				id: '0005',
+				imgUrl: 'https://imgs.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_250x250_6ec2f798.jpg',
+				title: '北京野生动物园',
+				desc: '敢与森林之王近距离接触吗？'
+			},]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+	margin-top: .2rem
+	line-height: .8rem
+	background: #eee
+	text-indent: .2rem
+.item
+	overflow: hidden
+	display: flex
+	height: 1.9rem
+	.item-img
+		width:1.7rem
+		height: 1.7rem
+		padding: .1rem
+	.item-info
+		flex: 1
+		padding: .1rem
+		min-width: 0
+		.item-title
+			line-height: .54rem
+			font-size: .32rem
+			ellipsis()
+		.item-desc
+			line-height: .4rem
+			color: #ccc
+			ellipsis()
+		.item-button
+			line-height:.44rem
+			margin-top: .16rem
+			background: #ff9300
+			padding: 0 .2rem
+			border-radius: .06rem
+			color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,63 @@
+<template>
+	<div>
+		<div class="title">周末去哪儿</div>
+		<ul>
+			<li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+				<div class="item-img-wrapper">
+					<img class="item-img" :src="item.imgUrl" />
+				</div>	
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name:'HomeWeekend',
+	data () {
+		return {
+			recommendList: [{
+				id: '0001',
+				imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/2cb12a625b4f5cb4d6d9a1f4cd307d65.jpg',
+				title: '北京野生动物园',
+				desc: '敢与森林之王近距离接触吗？'
+			},
+			{
+				id: '0002',
+				imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/2cb12a625b4f5cb4d6d9a1f4cd307d65.jpg',
+				title: '北京野生动物园',
+				desc: '敢与森林之王近距离接触吗？'
+			}]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+	margin-top: .2rem
+	line-height: .8rem
+	background: #eee
+	text-indent: .2rem
+.ietm-img-wrapper
+	overflow:hidden
+	height: 0
+	padding-bottom: 33.9%
+	.item-img
+		width: 100%
+.item-info
+	padding: .1rem
+	.item-title
+		line-height: .54rem
+		font-size: .32rem
+		ellipsis()
+	.item-desc
+		line-height: .4rem
+		color: #ccc
+		ellipsis()
+</style>
